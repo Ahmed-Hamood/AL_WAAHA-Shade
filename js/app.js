e8173a73eb3e1be2dbb129eede39189d3eeb78fa
Ahmed Hamood <ahmedalqourabah@gmail.com>
2023-06-12 09:13:01 +0400

    done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 import slideshow from "./slideshow.js";
 
 function Run() {
+  let navbar_items = document.querySelectorAll(".navbar-item");
   slideshow();
+
+  // active menu item when clicked
+  for (let i = 0; i < navbar_items.length; i++) {
+    navbar_items[i].addEventListener("click", (item) => {
+      for (let i = 0; i < navbar_items.length; i++) navbar_items[i].classList.remove("active");
+
+      if (item.target.tagName == "A") item.target.parentElement.classList.add("active");
+      else item.target.classList.add("active");
+    });
+  }
 }
 
-
-Run()
+Run();
