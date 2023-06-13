b0d9394e92b69808845baa607621c270466d706e
Ahmed Hamood <ahmedalqourabah@gmail.com>
2023-06-14 03:11:02 +0400

    done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 import slideshow from "./slideshow.js";
 
 function Run() {
+
+  let body_element = document.querySelector("body");
+
   let navbar_items = document.querySelectorAll(".navbar-item");
+  let product_list_links = document.querySelectorAll(".product-list-link");
+  let view_group_content = document.querySelectorAll(".view-group-content");
+
+  let products_list_container = document.querySelector(".products-list-container");
+  let products_view_items_container = document.querySelector(".products-view-items-container");
+
+  let back_btn = document.querySelector(".products-view-back-content");
+
   slideshow();
 
   // active menu item when clicked
@@ -13,6 +24,72 @@ function Run() {
       else item.target.classList.add("active");
     });
   }
+
+  if (window.innerWidth <= 855) {
+    for (let i = 0; i < product_list_links.length; i++) product_list_links[i].classList.remove("active");
+  }
+  // active product list links when clicked, and view products elements
+  for (let i = 0; i < product_list_links.length; i++) {
+    product_list_links[i].addEventListener("click", (item) => {
+      let selected_item = item.target.getAttribute("product_name");
+
+      for (let i = 0; i < product_list_links.length; i++) product_list_links[i].classList.remove("active");
+
+      item.target.classList.add("active");
+
+      view_group_content.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+
+      if (window.innerWidth <= 855) {
+        products_list_container.classList.add("no-display");
+        products_view_items_container.classList.add("active");
+        back_btn.classList.add("active");
+      } else {
+        products_view_items_container.classList.remove("no-display");
+      }
+
+      view_group_content.forEach((el) => {
+        if (selected_item == el.getAttribute("group_name")) {
+          el.classList.add("active");
+        }
+      });
+    });
+  }
+
+  // #################################
+  // Mobile
+  // #################################
+
+  // back btn active
+
+  back_btn.addEventListener("click", (ev) => {
+    products_list_container.classList.remove("no-display");
+    products_view_items_container.classList.remove("active");
+    back_btn.classList.remove("active");
+
+    for (let i = 0; i < product_list_links.length; i++) product_list_links[i].classList.remove("active");
+  });
+
+
+  // ---------------------
+  let HorPos = null
+
+  body_element.addEventListener(
+    "scroll",
+    event => {
+     HorPos = event.target.scrollTop
+    
+    //  if (window.innerWidth < 400) {
+      if (HorPos >= 330) {
+         alert("ffff")
+      } else {
+         alert("ffff")
+      }
+    //  }
+    },
+    { passive: true }
+  )
 }
 
 Run();
---
+++ js/ourProducts.js
@@ -0,0 +1,30 @@
+
+let products_info = [
+  {
+    id: 1,
+    title_arabic: "",
+    title_eng: "",
+    items: [
+      {
+        item_title_arabic: "",
+        item_title_eng: "",
+        item_images: [
+          "./img/product_view/../img-1.jpg",
+          "./img/product_view/../img-2.jpg",
+          "./img/product_view/../img-3.jpg",
+          "./img/product_view/../img-4.jpg",
+        ]
+      }
+    ]
+    
+  }
+]
+
+
+
+
+
+export default function ourProducts() { 
+
+
+}
